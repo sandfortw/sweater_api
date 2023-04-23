35677a1040cecb8f4220991654f5effda3d3be3e
Weston Sandfort <sandfortw@gmail.com>
2023-04-23 12:43:33 -0600

    finish tests for forecastservice

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -4,24 +4,17 @@ class ForecastService
     @info = info
   end
 
-  def generate_forecast
-    get_current_weather
-  end
-
-
-  def get_current_weather
+  def get_forecast
     response = conn.get do |req|
       req.url 'v1/forecast.json'
       req.params[:key] = ENV['WEATHER_API_KEY']
       req.params[:q] = "#{@info[:lat]}\,#{@info[:lng]}"
-      req.params[:days] = '1'
+      req.params[:days] = '5'
       req.params[:aqi] = "no"
       req.params[:alerts] = "no"
     end
     json = JSON.parse(response.body, symbolize_names: true)
   end
-
-
 
   private
 
--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -1,39 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe ForecastService, type: :service do
-  describe 'instance methods' do
+  describe 'instance methods', :vcr do
     before do
       @location = {lat: 39.10713, lng: -84.50413} #Cincinatti, Ohio
-      @service = ForecastService.new({lat: @location[:lat], lng: @location[:lng]})
+      @forecast = ForecastService.new({lat: @location[:lat], lng: @location[:lng]}).get_forecast
     end
-    describe '#get_current_weather', :vcr do
-      it 'should return data for current weather' do
-        current_weather = @service.get_current_weather
-        expect(current_weather).to be_a(Hash)
-        expect(current_weather[:current][:last_updated]).to be_a(String)
-        expect(current_weather[:current][:temp_f]).to be_a(Float)
-        expect(current_weather[:current][:feelslike_f]).to be_a(Float)
-        expect(current_weather[:current][:humidity]).to be_an(Integer)
-        expect(current_weather[:current][:uv]).to be_a(Float)
-        expect(current_weather[:current][:vis_miles]).to be_a(Float)
-        expect(current_weather[:current][:condition][:text]).to be_a(String)
-        expect(current_weather[:current][:condition][:icon]).to be_a(String)
+    describe '#get_forecast' do
+      it 'should return data for current weather (current_weather)' do
+        expect(@forecast).to be_a(Hash)
+        expect(@forecast[:current][:last_updated]).to be_a(String)
+        expect(@forecast[:current][:temp_f]).to be_a(Float)
+        expect(@forecast[:current][:feelslike_f]).to be_a(Float)
+        expect(@forecast[:current][:humidity]).to be_an(Integer)
+        expect(@forecast[:current][:uv]).to be_a(Float)
+        expect(@forecast[:current][:vis_miles]).to be_a(Float)
+        expect(@forecast[:current][:condition][:text]).to be_a(String)
+        expect(@forecast[:current][:condition][:icon]).to be_a(String)
       end
     end
 
-    describe '#get_daily_weather' do
-      # it 'should return data for daily weather' do
-      #   daily_weather = @service.get_daily_weather
-      #   expect(daily_weather).to be_a(Hash)
-      #   expect(daily_weather[:current][:last_updated]).to be_a(String)
-      #   expect(daily_weather[:current][:temp_f]).to be_a(Float)
-      #   expect(daily_weather[:current][:feelslike_f]).to be_a(Float)
-      #   expect(daily_weather[:current][:humidity]).to be_an(Integer)
-      #   expect(daily_weather[:current][:uv]).to be_a(Float)
-      #   expect(daily_weather[:current][:vis_miles]).to be_a(Float)
-      #   expect(daily_weather[:current][:condition][:text]).to be_a(String)
-      #   expect(daily_weather[:current][:condition][:icon]).to be_a(String)
-      # end
+    it 'should return forecast data for 5 days (daily_weather)' do
+      expect(@forecast[:forecast]).to be_a(Hash)
+      expect(@forecast[:forecast][:forecastday]).to be_an(Array)
+      expect(@forecast[:forecast][:forecastday].size).to eq(5)
+      expect(@forecast[:forecast][:forecastday].sample[:date]).to be_a(String)
+      expect(@forecast[:forecast][:forecastday].sample[:astro][:sunrise]).to be_a(String)
+      expect(@forecast[:forecast][:forecastday].sample[:astro][:sunset]).to be_a(String)
+      expect(@forecast[:forecast][:forecastday].sample[:day][:maxtemp_f]).to be_a(Float)
+      expect(@forecast[:forecast][:forecastday].sample[:day][:mintemp_f]).to be_a(Float)
+      expect(@forecast[:forecast][:forecastday].sample[:day][:condition][:text]).to be_a(String)
+      expect(@forecast[:forecast][:forecastday].sample[:day][:condition][:icon]).to be_a(String)
+    end
+
+    it 'should return hourly weather for the current day (hourly weather)' do
+      expect(@forecast[:forecast][:forecastday].sample[:hour]).to be_an(Array)
+      expect(@forecast[:forecast][:forecastday].sample[:hour].size).to be(24)
+      expect(@forecast[:forecast][:forecastday].sample[:hour].sample[:time]).to be_a(String)
+      expect(@forecast[:forecast][:forecastday].sample[:hour].sample[:temp_f]).to be_a(Float)
+      expect(@forecast[:forecast][:forecastday].sample[:hour].sample[:condition][:text]).to be_a(String)
+      expect(@forecast[:forecast][:forecastday].sample[:hour].sample[:condition][:icon]).to be_a(String)
     end
   end
 end
