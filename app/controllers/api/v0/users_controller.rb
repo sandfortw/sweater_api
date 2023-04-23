09770f5041c9e748f4897be75ed6c1658887c7ac
Weston Sandfort <sandfortw@gmail.com>
2023-04-23 17:04:58 -0600

    create user model and user controller

---
+++ app/controllers/api/v0/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+
+  def create
+    
+  end
+
+
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       get '/forecast', to: 'forecast#show'
+      post '/users', to: 'users#create'
     end
   end
 end
