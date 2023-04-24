25ba3e3e6296dd48736cfbd4f9e12e099e43b9b1
Weston Sandfort <sandfortw@gmail.com>
2023-04-24 17:11:36 -0600

    add http status tests to post user request

--- spec/requests/post_user_request_spec.rb
+++ spec/requests/post_user_request_spec.rb
@@ -20,5 +20,35 @@ RSpec.describe 'Users', type: :request do
       expect(returned[:data][:attributes][:email]).to eq(payload[:email])
       expect(returned[:data][:attributes][:api_key]).to be_a String
     end
+
+    it 'should return some error message if the payloads passwords do not match' do 
+      payload = {
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password1"
+      }
+
+      post "/api/v0/users", params: payload.to_json
+
+      returned = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(returned[:error]).to be_a String
+      expect(returned[:error].include?("Password confirmation doesn't match Password")).to be(true)
+    end
+
+    it 'should return a different error if a field is blank' do
+      payload = {
+        email: "",
+        password: "password",
+        password_confirmation: "password"
+      }
+
+      post "/api/v0/users", params: payload.to_json
+
+      returned = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(returned[:error]).to be_a String
+      expect(returned[:error].include?("Email can\'t be blank")).to be(true)
+    end
   end
 end
