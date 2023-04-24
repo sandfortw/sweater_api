88fa40c4b1420cacd0c25d0cfef883561be158e9
Weston Sandfort <sandfortw@gmail.com>
2023-04-24 10:05:42 -0600

    create test for salary request

--- spec/requests/salary_request_spec.rb
+++ spec/requests/salary_request_spec.rb
@@ -9,7 +9,22 @@ RSpec.describe "Salary", type: :request do
     end
 
     it 'should return the correct information' do
-      require 'pry'; binding.pry
+      expect(@salary).to be_a Hash
+      expect(@salary.keys).to contain_exactly(:data)
+      expect(@salary[:data]).to be_a Hash
+      expect(@salary[:data].keys).to contain_exactly(:id, :type, :attributes)
+      expect(@salary[:data][:id]).to eq(nil)
+      expect(@salary[:data][:type]).to eq("salaries")
+      expect(@salary[:data][:attributes]).to be_a Hash
+      expect(@salary[:data][:attributes].keys).to contain_exactly(:destination, :forecast, :salaries)
+      expect(@salary[:data][:attributes][:destination]).to eq("Chicago")
+      expect(@salary[:data][:attributes][:forecast][:summary]).to be_a(String)
+      expect(@salary[:data][:attributes][:forecast][:temperature]).to be_a(String)
+      expect(@salary[:data][:attributes][:salaries]).to be_an Array
+      expect(@salary[:data][:attributes][:salaries].sample).to be_a Hash
+      expect(@salary[:data][:attributes][:salaries].sample[:title]).to be_a String
+      expect(@salary[:data][:attributes][:salaries].sample[:min]).to be_a String
+      expect(@salary[:data][:attributes][:salaries].sample[:max]).to be_a String
     end
   end
 end
